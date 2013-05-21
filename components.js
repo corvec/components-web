90289cf4c252c581cce76dbe272706cde3516d32
Corey T Kump <ckump@fusionalliance.com>
2013-05-21 14:48:20 -0400

    Working on iPhone now.

--- components.js
+++ components.js
@@ -14,6 +14,9 @@ $(window).bind("load", function() {
 			};
 		}(data);
 	}
+
+	if(!('contains' in String.prototype))
+		String.prototype.contains = function(str, startIndex) { return -1!==this.indexOf(str, startIndex); };
 });
 
 function screen_width() {
@@ -31,6 +34,11 @@ function action_filter() {
 	return false;
 }
 
+function filter_row(row) {
+
+
+}
+
 // Set the visibility of tables in the document such that only those that match
 // the filter criteria are shown
 function filter_document(search) {
@@ -41,6 +49,8 @@ function filter_document(search) {
 	// * Type (Creation)
 	// * Power (1 or Common)
 	
+	// console.log("filter_document(" + search + ")");
+
 	var filters = search.split(' ');
 
 	var power_ary = ['common','uncommon','rare','singular','scarce','1','2','4'];
@@ -56,36 +66,61 @@ function filter_document(search) {
 	};
 	
 	var col_max = $('#component_table tr')[0].childNodes.length;
-	$('#component_table').children().each(function() {
-		var row = $(this);
+	$('#component_table').children().each(function(row_i, row) {
 		var hidden = false;
 		for (var i = 0; i < filters.length; i++) {
 			var crit = filters[i];
 			var filter_success = false;
 			if (crit in power_obj) {
-				console.log('Specifically checking for the power of the component');
+				// if (row_i == 0)
+				// 	console.log('Specifically checking the component power');
 				if (crit != 'common' && crit != 'uncommon') {// special cases
-					if (!(row.children()[3].textContent == power_obj[crit])) {
+					if (!(row.children[3].textContent == power_obj[crit])) {
 						hidden = true;
+						break;
 					}
 				} else { // common and uncommon
-					if (!(row.children()[1].textContent[0].toLowerCase() == crit[0])) {
+					if (!(row.children[1].textContent[0].toLowerCase() == crit[0])) {
 						hidden = true;
+						break;
 					}
 				}
 			} else {
+				// if (row_i < 10)
+				// 	console.log('Checking filter "' + crit + '" in row ' + row_i);
 				for (var j = 1; j < col_max; j++) {
-					if (row.children()[j].textContent.toLowerCase().contains(crit)) {
+					if (row.children[j].textContent.toLowerCase().contains(crit)) {
 						filter_success = true;
+						// if (row_i < 10)
+						// 	console.log('filter found in row ' + row_i);
 						break;
 					}
 				}
 				if (!filter_success) {
 					hidden = true;
+					// if (row_i < 10)
+					// 	console.log('filter not found in row ' + row_i);
+					break;
 				}
 			}
 		}
-		if (hidden) { row.hide(); } else { row.show(); }
+		if (hidden) {
+			// if (row_i < 10)
+			// 	console.log('Hiding row ' + row_i);
+			if (typeof(row.hide) === typeof(Function)) {
+				row.hide();
+			} else {
+				row.style.display = 'none';
+			}
+		} else {
+			// if (row_i < 10)
+			// 	console.log('Showing row ' + row_i);
+			if (typeof(row.show) === typeof(Function)) {
+				row.show();
+			} else {
+				row.style.display = '';
+			}
+		}
 	});
 
 
